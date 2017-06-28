427524df6fdc24955336d090970b8f3e9597b808
cllorca1 <carlos.llorca@tum.de>
2017-06-28 14:56:35 +0200

    extension on compare travel time matrices done

--- ttMatrix/compareTt.R
+++ ttMatrix/compareTt.R
@@ -5,6 +5,7 @@
 
 library(ggplot2)
 library(reshape2)
+library(dplyr)
 
 #set wd to read matrices
 setwd("C:/models/munich/data/1.000.75")
@@ -97,18 +98,33 @@ sum(dif10$value^2)/nrow(dif1)
 sum(dif20$value^2)/nrow(dif1)
 sum(dif50$value^2)/nrow(dif1)
 
+# dif1$run = "simple1"
+# dif5$run = "simple5"
+# dif10$run = "simple10"
+# dif20$run = "simple20"
+# dif50$run = "simple50"
 
-dif1 =   melt(as.data.frame(- matrixList[[7]] + matrixList[[12]]),measure.vars = 1:4953)
-dif5 =   melt(as.data.frame(- matrixList[[8]] + matrixList[[12]]),measure.vars = 1:4953)
-dif10 =  melt(as.data.frame(- matrixList[[9]] + matrixList[[12]]),measure.vars = 1:4953)
-dif20 =  melt(as.data.frame(- matrixList[[10]] + matrixList[[12]]),measure.vars = 1:4953)
-dif50 =  melt(as.data.frame(- matrixList[[11]] + matrixList[[12]]),measure.vars = 1:4953)
+##be carefull with runtime, this is too long
 
-sum(dif1$value^2)/nrow(dif1)
-sum(dif5$value^2)/nrow(dif1)
-sum(dif10$value^2)/nrow(dif1)
-sum(dif20$value^2)/nrow(dif1)
-sum(dif50$value^2)/nrow(dif1)
+allDifSimple = rbind(dif1, dif5, dif10, dif20, dif50)
+
+ggplot(allDifSimple,aes(x=value, fill=run, color=run)) + stat_density(alpha = .1)+ xlim(-100,100)
+
+
+dif1f =   melt(as.data.frame(- matrixList[[7]] + matrixList[[12]]),measure.vars = 1:4953)
+dif5f =   melt(as.data.frame(- matrixList[[8]] + matrixList[[12]]),measure.vars = 1:4953)
+dif10f =  melt(as.data.frame(- matrixList[[9]] + matrixList[[12]]),measure.vars = 1:4953)
+dif20f =  melt(as.data.frame(- matrixList[[10]] + matrixList[[12]]),measure.vars = 1:4953)
+dif50f =  melt(as.data.frame(- matrixList[[11]] + matrixList[[12]]),measure.vars = 1:4953)
+
+sum(dif1f$value^2)/nrow(dif1)
+sum(dif5f$value^2)/nrow(dif1)
+sum(dif10f$value^2)/nrow(dif1)
+sum(dif20f$value^2)/nrow(dif1)
+sum(dif50f$value^2)/nrow(dif1)
+
+
+
 
 #plot all the errors // TEST ONLY
 # setwd("C:/projects/MATSim/scaling/analysis/ttODPairs")
@@ -121,4 +137,58 @@ sum(dif50$value^2)/nrow(dif1)
 # image((- matrixList[[5]] + matrixList[[6]]), axes = T, useRaster = T, col = rainbow(5))
 # dev.off()
 
+#caluclate average errors by origin: 
 
+setwd("C:/projects/MATSim/scaling/analysis/ttODPairs")
+
+dif1 = as.data.frame(- matrixList[[1]] + matrixList[[6]])
+dif1Agg = dif1 %>% summarize_all(mean)
+dif5 = as.data.frame(- matrixList[[2]] + matrixList[[6]])
+dif5Agg = dif5 %>% summarize_all(mean)
+dif10 = as.data.frame(- matrixList[[3]] + matrixList[[6]])
+dif10Agg = dif10 %>% summarize_all(mean)
+dif20 = as.data.frame(- matrixList[[4]] + matrixList[[6]])
+dif20Agg = dif20 %>% summarize_all(mean)
+dif50 = as.data.frame(- matrixList[[5]] + matrixList[[6]])
+dif50Agg = dif50 %>% summarize_all(mean)
+
+difs = rbind(dif1Agg, dif5Agg, dif10Agg, dif20Agg, dif50Agg)
+row.names(difs) = c("s1", "s5", "s10", "s20", "s50")
+difs = as.data.frame(t(difs))
+write.csv(t(difs), file = "simpleNetworkErrors.csv")
+
+dif1 = as.data.frame(- matrixList[[7]] + matrixList[[12]])
+dif1Agg = dif1 %>% summarize_all(mean)
+dif5 = as.data.frame(- matrixList[[8]] + matrixList[[12]])
+dif5Agg = dif5 %>% summarize_all(mean)
+dif10 = as.data.frame(- matrixList[[9]] + matrixList[[12]])
+dif10Agg = dif10 %>% summarize_all(mean)
+dif20 = as.data.frame(- matrixList[[10]] + matrixList[[12]])
+dif20Agg = dif20 %>% summarize_all(mean)
+dif50 = as.data.frame(- matrixList[[11]] + matrixList[[12]])
+dif50Agg = dif50 %>% summarize_all(mean)
+
+difs = rbind(dif1Agg, dif5Agg, dif10Agg, dif20Agg, dif50Agg)
+row.names(difs) = c("s1f", "s5f", "s10f", "s20f", "s50f")
+difs = as.data.frame(t(difs))
+write.csv(difs, file = "fullNetworkErrors.csv")
+
+difs$id = c(1:4953)
+difsLong = melt(difs, id.vars = "id")
+ggplot(difsLong,aes(x=value, fill=variable, color=variable)) + stat_density(alpha = .1)+ xlim(-10,10)
+
+
+#compare across networks given the same number of agents
+
+
+scalingFactorOrder = 6
+
+tt1s = melt(as.data.frame(matrixList[[scalingFactorOrder]]),measure.vars = 1:4953)
+tt1f = melt(as.data.frame(matrixList[[scalingFactorOrder+1]]),measure.vars = 1:4953)
+
+totalRows = 4953^2
+dif1All = data.frame(id = c(1:totalRows), simple = tt1s$value, full = tt1f$value)
+selectedRows = sample(1:totalRows,10000,replace=F) 
+dif1All = subset(dif1All, id %in% selectedRows)
+
+ggplot(dif1All,aes(x=simple, y=full)) + geom_point() + geom_abline(slope=1, intercept = 0, color = "blue")
